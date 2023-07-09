6a699557ce8c3fe8b0cf67f17a7dd5a6eb13fe5b
hgarni <hgarni@hotmail.com>
2023-07-09 00:06:55 -0300

    modificacion en rutas de scripts en 11A_601_RUN_correr_workflow_CV.r

--- src/11A_601_RUN_correr_workflow_CV.r
+++ src/11A_601_RUN_correr_workflow_CV.r
@@ -10,11 +10,11 @@ options(error = function() {
 # corrida de cada paso del workflow
 
 # primeros pasos, relativamente rapidos
-source("~/src/11A_611_CA_reparar_dataset.r")
-source("~/src/11A_621_DR_corregir_drifting.r")
-source("~/src/workflow-inicial/11A_631_FE_historia.r")
-source("~/src/workflow-inicial/11A_641_TS_training_strategy_CV.r")
+source("~/cv-wfv/src/11A_611_CA_reparar_dataset.r")
+source("~/cv-wfv/src/11A_621_DR_corregir_drifting.r")
+source("~/cv-wfv/src/workflow-inicial/11A_631_FE_historia.r")
+source("~/cv-wfv/src/workflow-inicial/11A_641_TS_training_strategy_CV.r")
 
 # ultimos pasos, muy lentos
-source("~/src/11A_651_HT_lightgbm_CV.r")
-source("~/src/11A_661_ZZ_final_CV.r")
+source("~/cv-wfv/src/11A_651_HT_lightgbm_CV.r")
+source("~/cv-wfv/src/11A_661_ZZ_final_CV.r")
--- src/11A_601_RUN_correr_workflow_WFV.r
+++ src/11A_601_RUN_correr_workflow_WFV.r
@@ -10,15 +10,15 @@ options(error = function() {
 # corrida de cada paso del workflow
 
 # primeros pasos, relativamente rapidos
-source("~/labo2023r/src/workflow-inicial/11A_611_CA_reparar_dataset.r")
-source("~/labo2023r/src/workflow-inicial/11A_621_DR_corregir_drifting.r")
-source("~/labo2023r/src/workflow-inicial/11A_631_FE_historia.r")
-source("~/labo2023r/src/workflow-inicial/11A_641_TS_training_strategy_WFC_01.r")
-#source("~/labo2023r/src/workflow-inicial/11A_641_TS_training_strategy_WFC_02.r")
-#source("~/labo2023r/src/workflow-inicial/11A_641_TS_training_strategy_WFC_03.r")
-#source("~/labo2023r/src/workflow-inicial/11A_641_TS_training_strategy_WFC_04.r")
-#source("~/labo2023r/src/workflow-inicial/11A_641_TS_training_strategy_WFC_05.r")
+source("~/cv-wfv/src/11A_611_CA_reparar_dataset.r")
+source("~/cv-wfv/src/11A_621_DR_corregir_drifting.r")
+source("~/cv-wfv/src/11A_631_FE_historia.r")
+source("~/cv-wfv/src/11A_641_TS_training_strategy_WFC_01.r")
+#source("~/cv-wfv/src/11A_641_TS_training_strategy_WFC_02.r")
+#source("~/cv-wfv/src/11A_641_TS_training_strategy_WFC_03.r")
+#source("~/cv-wfv/src/11A_641_TS_training_strategy_WFC_04.r")
+#source("~/cv-wfv/src/11A_641_TS_training_strategy_WFC_05.r")
 
 # ultimos pasos, muy lentos
-source("~/labo2023r/src/workflow-inicial/11A_651_HT_lightgbm_WFC.r")
-source("~/labo2023r/src/workflow-inicial/11A_661_ZZ_final_WFV.r")
+source("~/cv-wfv/src/11A_651_HT_lightgbm_WFC.r")
+source("~/cv-wfv/src/11A_661_ZZ_final_WFV.r")
