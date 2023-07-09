ff017feb1a2943567131106772ec38190460d7e3
hgarni <hgarni@hotmail.com>
2023-07-09 00:32:05 -0300

    modificacion en rutas de scripts en 11A_601_RUN_correr_workflow_CV.r

--- src/11A_601_RUN_correr_workflow_CV.r
+++ src/11A_601_RUN_correr_workflow_CV.r
@@ -12,8 +12,8 @@ options(error = function() {
 # primeros pasos, relativamente rapidos
 source("~/cv-wfv/src/11A_611_CA_reparar_dataset.r")
 source("~/cv-wfv/src/11A_621_DR_corregir_drifting.r")
-source("~/cv-wfv/src/workflow-inicial/11A_631_FE_historia.r")
-source("~/cv-wfv/src/workflow-inicial/11A_641_TS_training_strategy_CV.r")
+source("~/cv-wfv/src/11A_631_FE_historia.r")
+source("~/cv-wfv/src/11A_641_TS_training_strategy_CV.r")
 
 # ultimos pasos, muy lentos
 source("~/cv-wfv/src/11A_651_HT_lightgbm_CV.r")
